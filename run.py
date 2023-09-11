227f422761c03bab7a11e022f1d9866ff4b4c08d
ireneuszcierpisz <>
2023-09-11 22:20:21 +0000

    Add user request loop to repeat request for data until data provided is valid

--- run.py
+++ run.py
@@ -1,17 +1,6 @@
-"""
-imports the entire gspread library  
-so that we can access any function,  class or method within it. 
-""" 
 import gspread
-
-"""
-imports the Credentials class,  
-which is part of the service_account  function from the Google auth library.  
-As we only need this class for our project, there  is no need to import the entire library here.
-"""
 from google.oauth2.service_account import Credentials
 
-# the APIs that the  program should access
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
@@ -23,22 +12,25 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
-#checks our API is working
-# sales = SHEET.worksheet('sales')
-# data = sales.get_all_values()
-# print(data)
 
 def get_sales_data():
     """
     Get sales figures input from the user.
     """
-    print("Please enter sales data from the last market.")
-    print("Data should be six numbers, separated by commas.")
-    print("Example: 10,20,30,40,50,60\n")
+    while True:
+        print("Please enter sales data from the last market.")
+        print("Data should be six numbers, separated by commas.")
+        print("Example: 10,20,30,40,50,60\n")
 
-    data_str = input("Enter your data here: ")
-    sales_data = data_str.split(",")
-    validate_data(sales_data)
+        data_str = input("Enter your data here: ")
+
+        sales_data = data_str.split(",")
+
+        if validate_data(sales_data):
+            print("Data is valid!")
+            break
+
+    return sales_data
 
 
 def validate_data(values):
@@ -55,5 +47,9 @@ def validate_data(values):
             )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
+        return False
 
-get_sales_data()
+    return True
+
+
+data = get_sales_data()
